dbcab505bfbeccd69085b3feef033f590e4ab642
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-07-17 18:50:40 +0300

    fix: text underline

--- src/components/layout/Header.svelte
+++ src/components/layout/Header.svelte
@@ -1,13 +1,13 @@
 <script>
-	import { Container, Flex, Title, Group, Anchor } from '@svelteuidev/core';
+	import { Container, Flex, Title, Group } from '@svelteuidev/core';
 	import ThemeToggle from '$components/layout/components/header/ThemeToggle.svelte';
 </script>
 
 <Container fluid class="p-8">
 	<Flex justify="space-between">
-		<Anchor href="/" class="no-underline hover:no-underline">
-			<Title order={2} class="hover:text-slate-600">Podcast.io</Title>
-		</Anchor>
+		<a href="/" class="no-underline hover:no-underline">
+			<Title order={1} class="hover:text-slate-600">Podcast.io</Title>
+		</a>
 		<Group>
 			<ThemeToggle />
 		</Group>
