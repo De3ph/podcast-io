2933953062a40f57d449851d8ffd10784ec94530
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-07-18 10:40:14 +0300

    feat: id routes

---
+++ src/routes/podcast/[id]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+    console.log('====================================');
+    console.log(data);
+    console.log('====================================');
+</script>
+
+<main>
+    {data.id}
+</main>
