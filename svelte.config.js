53ea983ee1c5c936d89b8a9a74c3dadb30612a0a
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-07-20 00:13:31 +0300

    feat: new paths, env prefix config

--- svelte.config.js
+++ svelte.config.js
@@ -7,13 +7,18 @@ const config = {
 	// for more information about preprocessors
 
 	kit: {
+		env: {
+			dir: '/',
+			privatePrefix: 'PRIVATE_'
+		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
 			'$components/*': 'src/components/*',
-			'$utils/*': 'src/utils/*'
+			'$utils/*': 'src/utils/*',
+			'$lib/*': 'src/lib/*'
 		}
 	},
 	preprocess: vitePreprocess()
