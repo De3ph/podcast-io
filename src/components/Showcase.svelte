d0230120260031e4e35ce6c5c8165225b461b95b
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-07-20 00:15:51 +0300

    fix: little fixes

--- src/components/Showcase.svelte
+++ src/components/Showcase.svelte
@@ -5,11 +5,11 @@
 
 <Box class="w-full sm:w-2/3 md:w-3/5 lg:w-1/2 mx-auto mb-15">
 	<Grid
-		class="font-serif font-medium md:text-2xl lg:text-4xl
+		class="font-serif font-medium md:text-2xl lg:text-4xl space-y-7
         "
 	>
 		<ScItem img="/podcast_grid_1.jpg" text="Lorem ipsum dolor sit amet consectetur." />
 
-		<ScItem img="/podcast_grid_2.jpg" text="Lorem ipsum dolor sit amet consectetur." />
+		<ScItem reverse={true} img="/podcast_grid_2.jpg" text="Lorem ipsum dolor sit amet consectetur." />
 	</Grid>
 </Box>
