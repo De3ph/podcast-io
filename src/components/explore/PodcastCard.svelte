b6de60885227a02996de86c6502502ef8df0b256
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-07-20 00:15:33 +0300

    feat: new component

---
+++ src/components/explore/PodcastCard.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Card, Group, Image, Stack, Text, Title } from '@svelteuidev/core';
+	export let podcast: IPodcast;
+
+	const href = `/podcast/${podcast.id}`
+</script>
+
+<a {href}>
+	<Card class="h-full space-y-3">
+		<Card.Section padding="lg">
+			<Image src="/podcast_grid_1.jpg" height={150} alt="cover" usePlaceholder class="object-cover" />
+		</Card.Section>
+	
+			<Stack class="px-1 pt-2">
+				<Title order={2}>{podcast.name}</Title>
+		
+				<Group class="px-4" position="right">
+					<Text>
+						{podcast.author}
+					</Text>
+				</Group>
+			</Stack>
+	</Card>
+</a>
