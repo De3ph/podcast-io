bbb780606573511a1bede904a2c4a12a1dad3f7b
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-07-20 00:36:16 +0300

    fix: ?

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-	import.meta.env.VITE_PRIVATE_SUPABASE_URL,
-	import.meta.env.VITE_PRIVATE_SUPABASE_API_KEY,
+	process.env.VITE_PRIVATE_SUPABASE_URL,
+	process.env.VITE_PRIVATE_SUPABASE_API_KEY,
 	{
 		auth: {
 			persistSession: false
