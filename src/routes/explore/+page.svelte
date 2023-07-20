f88dbb0455a9e1e95710e50bbeab0a8a725f9264
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-07-20 11:35:26 +0300

    fix: fix title

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -1,32 +1,45 @@
 <script lang="ts">
-    import PodcastCard from '$components/explore/PodcastCard.svelte';
-	import { SimpleGrid } from '@svelteuidev/core';
-import type { PageData } from './$types';
-    
-    export let data: PageData;
+	import PodcastCard from '$components/explore/PodcastCard.svelte';
+	import { Center, SimpleGrid, Title } from '@svelteuidev/core';
+	import type { PageData } from './$types';
 
-    const {podcasts} = data;
+	export let data: PageData;
+
+	const { podcasts } = data;
 </script>
 
-<main>
-    Explore
+<main class="space-y-16">
+	<Center>
+		<p class="text-3xl">
+			Explore the <span
+				class="font-semibold bg-clip-text text-transparent bg-gradient-to-r from-slate-400 to-slate-800"
+				>World</span
+			> of podcasts
+		</p>
+	</Center>
 
-    <SimpleGrid class="px-6 sm:px-8 md:px-12 xl:px-24 gap-x-12 gap-y-16" breakpoints={[
-        { minWidth: 1536, cols: 5 },
-        { minWidth: 1023, cols: 3 },
-        { minWidth: 767, cols: 2 },
-        { minWidth: 639, cols: 1 }
-    ]}
-   
-    >
-        {#each podcasts as podcast}
-        <PodcastCard {podcast} />
-        <PodcastCard {podcast} />
-        <PodcastCard {podcast} />
-        <PodcastCard {podcast} />
-        <PodcastCard {podcast} />
-        <PodcastCard {podcast} />
-        <PodcastCard {podcast} />
-    {/each}
-    </SimpleGrid>
+	<SimpleGrid
+		class="px-6 sm:px-8 md:px-12 xl:px-24 gap-x-12 gap-y-16"
+		breakpoints={[
+			{ minWidth: 1536, cols: 5 },
+			{ minWidth: 1023, cols: 3 },
+			{ minWidth: 767, cols: 2 },
+			{ minWidth: 639, cols: 1 }
+		]}
+	>
+		{#each podcasts as podcast}
+				<PodcastCard {podcast} />
+				<PodcastCard {podcast} />
+				<PodcastCard {podcast} />
+				<PodcastCard {podcast} />
+				<PodcastCard {podcast} />
+				<PodcastCard {podcast} />
+				<PodcastCard {podcast} />
+				<PodcastCard {podcast} />
+				<PodcastCard {podcast} />
+				<PodcastCard {podcast} />
+				<PodcastCard {podcast} />
+				<PodcastCard {podcast} />
+		{/each}
+	</SimpleGrid>
 </main>
