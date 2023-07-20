d3bd15e2c07136026e0e35dbf407ed1ab2611eaf
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-07-20 11:35:43 +0300

    feat: add animation

--- src/components/explore/PodcastCard.svelte
+++ src/components/explore/PodcastCard.svelte
@@ -1,24 +1,27 @@
 <script lang="ts">
-	import { Card, Group, Image, Stack, Text, Title } from '@svelteuidev/core';
+	import FadeIn from '$components/wrappers/FadeIn.svelte';
+import { Card, Group, Image, Stack, Text, Title } from '@svelteuidev/core';
 	export let podcast: IPodcast;
 
 	const href = `/podcast/${podcast.id}`
 </script>
 
 <a {href}>
-	<Card class="h-full space-y-3">
-		<Card.Section padding="lg">
-			<Image src="/podcast_grid_1.jpg" height={150} alt="cover" usePlaceholder class="object-cover" />
-		</Card.Section>
-	
-			<Stack class="px-1 pt-2">
-				<Title order={2}>{podcast.name}</Title>
+	<FadeIn>
+		<Card class="h-full space-y-3 shadow-sm shadow-gray-300">
+			<Card.Section padding="lg">
+				<Image src="/podcast_grid_1.jpg" height={150} alt="cover" usePlaceholder class="object-cover" />
+			</Card.Section>
 		
-				<Group class="px-4" position="right">
-					<Text>
-						{podcast.author}
-					</Text>
-				</Group>
-			</Stack>
-	</Card>
+				<Stack class="px-1 pt-2">
+					<Title order={2}>{podcast.name}</Title>
+			
+					<Group class="px-4" position="right">
+						<Text>
+							{podcast.author}
+						</Text>
+					</Group>
+				</Stack>
+		</Card>
+	</FadeIn>
 </a>
