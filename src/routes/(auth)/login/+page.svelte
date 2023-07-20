59ebfeb8537571046de056e37f2f3311915c2e60
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-07-20 11:36:02 +0300

    feat: add chema validation

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -2,17 +2,26 @@
 	import FadeIn from '$components/wrappers/FadeIn.svelte';
 	import { supabase } from '$lib/supabaseClient';
 	import { Button, Stack, TextInput, Container } from '@svelteuidev/core';
+	import loginSchema from '../../../schemas/LoginSchema';
 	let email: string;
 	let password: string;
 
 	const handleClick = async () => {
-		const { data, error } = await supabase.auth.signInWithPassword({
-			email,
-			password
-		});
+		const credentials = { email, password };
+		const zodResult = loginSchema.safeParse(credentials)
+		if (zodResult.success) {
+			const { data, error } = await supabase.auth.signInWithPassword({
+				email,
+				password
+			});
 
-		if (data?.session?.access_token) {
-			alert('logged');
+			if (data?.session?.access_token) {
+				alert('logged');
+			}else{
+				console.log("🚀 ~ file: +page.svelte:21 ~ handleClick ~ error:", error)
+			}
+		}else{
+			alert(zodResult.error)
 		}
 	};
 </script>
